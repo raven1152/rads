ff95fdf4e6b640b762f0f9d181d373aaca9b6e64
Preston, David <dpreston@harsco.com>
2018-03-09 16:35:40 -0600

    .

---
+++ rads_db/api/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Chart(models.Model):
+    """
+    defines a chart in the system
+    """
+
+
+class ChartDataStructure(models.Model):
+    """
+    Chart Data
+    """
+
+
+class ChartDataInstance(models.Model):
+    """
+    Chart Data
+    """
+
+
+class SingleSeries(models.Model):
+    """
+    Single Series chart data
+    """
